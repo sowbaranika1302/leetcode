612cce2a2beb5afd9efa75ed594b5c618db81bae
Sowbaranika <balasubramaniam.29@osu.edu>
2023-02-07 18:53:32 -0500

    Time: 34 ms (90.43%), Space: 13.9 MB (25.23%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -8,7 +8,7 @@ class Solution:
         dummy = ListNode()
         tail = dummy
         while list1 and list2:
-            if list1.val < list2.val:
+            if list1.val<list2.val:
                 tail.next = list1
                 list1 = list1.next
             else:
@@ -17,10 +17,7 @@ class Solution:
             tail = tail.next
         if list1:
             tail.next = list1
-        elif list2:
+        if list2:
             tail.next = list2
-
-
         return dummy.next
-
-        
+                
