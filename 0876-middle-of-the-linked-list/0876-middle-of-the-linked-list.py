fe96d8d536d8145a21d2f7ac6a9238fc4eaf8de4
Sowbaranika <balasubramaniam.29@osu.edu>
2023-02-07 19:25:50 -0500

    Time: 31 ms (77.01%), Space: 13.9 MB (7.71%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -8,6 +8,6 @@ class Solution:
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
-            fast=fast.next.next
+            fast = fast.next.next
         return slow
         
