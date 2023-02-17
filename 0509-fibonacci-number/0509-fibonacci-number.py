a01283bcf50edb831bdb454017d61cc8b39eb0fe
Sowbaranika <balasubramaniam.29@osu.edu>
2023-02-16 21:59:31 -0500

    Time: 36 ms (57.81%), Space: 13.8 MB (93.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,4 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
+        if n<2:
+            return n
         sqrt5 = sqrt(5)
         return int((pow(1 + sqrt5, n) - pow(1 - sqrt5, n)) / pow(2, n) / sqrt5)
