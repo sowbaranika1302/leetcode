4afb8ef4291889a2499f78403f8352c95a34454e
Sowbaranika <balasubramaniam.29@osu.edu>
2023-02-16 21:51:57 -0500

    Time: 724 ms (26.77%), Space: 13.8 MB (44.80%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,10 +2,5 @@ class Solution:
     def fib(self, n: int) -> int:
         if n<2:
             return n
-        first = 0
-        second = 1
-        for i in range(0,n-1):
-            third = first+second
-            first = second
-            second = third
-        return third
+        
+        return self.fib(n-1)+ self.fib(n-2)
