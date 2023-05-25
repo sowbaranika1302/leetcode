8e4159c1f8d2a49a79d46a34aa198e9d49fa1789
Sowbaranika <balasubramaniam.29@osu.edu>
2023-05-25 00:34:29 -0400

    Time: 68 ms (34.59%), Space: 16.7 MB (52.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        dic = {}
+        if len(s)!=len(t):
+            return False
+        for i in s:
+            dic[i] = dic.get(i,0)+1
+        for i in t:
+            if i not in dic or dic.get(i)==0:
+                return False
+            dic[i] = dic.get(i)-1
+        return True
         
